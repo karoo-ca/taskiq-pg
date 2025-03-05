faf0541b0bed286bb337add4f580c8f32e1d5ad9
Oliver Lambson <ollie@karoo.ca>
2025-03-05 18:51:07 -0500

    dev: examples (#22)

---
+++ examples/webapp/src/webapp/worker.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+import taskiq_fastapi  # type: ignore[import-untyped]
+from taskiq.cli.worker.args import WorkerArgs
+from taskiq.cli.worker.run import run_worker
+from taskiq.serializers import JSONSerializer
+from that_depends import Provide, container_context, inject
+
+from taskiq_pg import AsyncpgBroker, AsyncpgResultBackend
+from webapp.dependencies import Dependencies
+from webapp.settings import Settings
+
+logger = logging.getLogger(__name__)
+
+result_backend: AsyncpgResultBackend[object] = AsyncpgResultBackend(
+    dsn=Dependencies.settings.sync_resolve().db_url,
+    serializer=JSONSerializer(),
+)
+broker = AsyncpgBroker(
+    dsn=Dependencies.settings.sync_resolve().db_url
+).with_result_backend(result_backend)
+
+taskiq_fastapi.init(broker, "webapp.webapp:make_app")
+
+
+@broker.task()
+@container_context()
+@inject()
+async def so_much_effort(settings: Settings = Provide[Dependencies.settings]) -> str:
+    """Worker task."""
+    logger.info("starting so_much_effort (using db=%s)", settings.PGDATABASE)
+    await asyncio.sleep(1)
+    logger.info("completed so_much_effort")
+    return "no more work, please."
+
+
+def entrypoint() -> int | None:
+    """Entrypoint to run worker."""
+    logging.basicConfig(level=logging.INFO)
+
+    args = WorkerArgs(broker="webapp.worker:broker", modules=[], workers=1)
+    return run_worker(args)
+    # NOTE: logging of worker tasks doesn't go to stdout for some reason?
