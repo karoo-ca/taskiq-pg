96533b63b1580c6c28195c2361824456a57ded8f
Oliver Lambson <oliverlambson@gmail.com>
2025-03-05 22:19:05 -0500

    chore(main): release 0.1.7

--- taskiq_pg/__init__.py
+++ taskiq_pg/__init__.py
@@ -1,7 +1,7 @@
 from taskiq_pg.broker import AsyncpgBroker
 from taskiq_pg.result_backend import AsyncpgResultBackend
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 __all__ = [
     "AsyncpgBroker",
